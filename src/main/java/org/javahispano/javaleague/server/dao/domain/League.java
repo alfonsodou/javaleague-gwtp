24ece26b4993417c30c85c04e84b8905eb8d438c
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2015-12-28 23:35:51 +0100

    Añadidos campos en league y leagueDto

--- src/main/java/org/javahispano/javaleague/server/dao/domain/League.java
+++ src/main/java/org/javahispano/javaleague/server/dao/domain/League.java
@@ -3,11 +3,19 @@
  */
 package org.javahispano.javaleague.server.dao.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.javahispano.javaleague.server.dao.objectify.Deref;
 import org.javahispano.javaleague.shared.dto.BaseEntity;
 import org.javahispano.javaleague.shared.dto.LeagueDto;
+import org.javahispano.javaleague.shared.dto.MatchDto;
 
+import com.google.gwt.thirdparty.guava.common.collect.Lists;
+import com.googlecode.objectify.Ref;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Load;
 
 /**
  * @author alfonso
@@ -17,13 +25,23 @@ import com.googlecode.objectify.annotation.Index;
 @Entity
 public class League extends BaseEntity {
 	private String description;
-	
+	private int roundMax;
+	private int round;
+
+	@Load
+	private List<List<Ref<Match>>> matchs;
+
 	public League() {
 		this.description = "";
+		this.roundMax = 0;
+		this.round = 0;
+		this.matchs = Lists.newArrayList();
 	}
-	
-	public League(String description) {
+
+	public League(String description, int roundMax, int round) {
 		this.description = description;
+		this.roundMax = roundMax;
+		this.round = round;
 	}
 
 	public String getDescription() {
@@ -33,15 +51,78 @@ public class League extends BaseEntity {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
+	/**
+	 * @return the roundMax
+	 */
+	public int getRoundMax() {
+		return roundMax;
+	}
+
+	/**
+	 * @param roundMax
+	 *            the roundMax to set
+	 */
+	public void setRoundMax(int roundMax) {
+		this.roundMax = roundMax;
+	}
+
+	/**
+	 * @return the round
+	 */
+	public int getRound() {
+		return round;
+	}
+
+	/**
+	 * @param round
+	 *            the round to set
+	 */
+	public void setRound(int round) {
+		this.round = round;
+	}
+
+	/**
+	 * @return the matchs
+	 */
+	public List<List<Ref<Match>>> getMatchs() {
+		return matchs;
+	}
+
+	/**
+	 * @param matchs
+	 *            the matchs to set
+	 */
+	public void setMatchs(List<List<Ref<Match>>> matchs) {
+		this.matchs = matchs;
+	}
+
 	public static LeagueDto createDto(League league) {
 		if (league == null) {
 			return null;
 		}
-		
+
 		LeagueDto leagueDto = new LeagueDto();
 		leagueDto.setId(league.getId());
-		
+		leagueDto.setRoundMax(league.getRoundMax());
+		leagueDto.setRound(league.getRound());
+		leagueDto.setMatchs(createListMatchs(league));
+
 		return leagueDto;
+	}
+
+	private static List<List<MatchDto>> createListMatchs(League league) {
+		List<List<MatchDto>> matchs = Lists.newArrayList();
+		List<MatchDto> matchsDto = new ArrayList<MatchDto>();
+
+		for (List<Ref<Match>> refMatch : league.getMatchs()) {
+			for (Ref<Match> match : refMatch) {
+				matchsDto.add(Match.createDto(Deref.deref(match)));
+			}
+			matchs.add(matchsDto);
+			matchsDto = new ArrayList<MatchDto>();
+		}
+
+		return matchs;
 	}
 }
--- src/main/java/org/javahispano/javaleague/shared/dto/LeagueDto.java
+++ src/main/java/org/javahispano/javaleague/shared/dto/LeagueDto.java
@@ -3,12 +3,18 @@
  */
 package org.javahispano.javaleague.shared.dto;
 
+import java.util.List;
+
 /**
  * @author adou
  *
  */
 public class LeagueDto extends BaseEntity {
 	private String description;
+	private int roundMax;
+	private int round;
+	
+	private List<List<MatchDto>> matchs;
 	
 	public LeagueDto() {
 		this.description = "";
@@ -24,6 +30,48 @@ public class LeagueDto extends BaseEntity {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	/**
+	 * @return the roundMax
+	 */
+	public int getRoundMax() {
+		return roundMax;
+	}
+
+	/**
+	 * @param roundMax the roundMax to set
+	 */
+	public void setRoundMax(int roundMax) {
+		this.roundMax = roundMax;
+	}
+
+	/**
+	 * @return the round
+	 */
+	public int getRound() {
+		return round;
+	}
+
+	/**
+	 * @param round the round to set
+	 */
+	public void setRound(int round) {
+		this.round = round;
+	}
+
+	/**
+	 * @return the matchs
+	 */
+	public List<List<MatchDto>> getMatchs() {
+		return matchs;
+	}
+
+	/**
+	 * @param matchs the matchs to set
+	 */
+	public void setMatchs(List<List<MatchDto>> matchs) {
+		this.matchs = matchs;
 	}
 	
 	
