d71c42abc52fb28e59acf71a5191554d30a7e3c0
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2016-01-13 21:38:04 +0100

    Añadido servlet ClasificationServlet

--- src/main/java/org/javahispano/javaleague/server/servlet/ClasificationServlet.java
+++ src/main/java/org/javahispano/javaleague/server/servlet/ClasificationServlet.java
@@ -3,10 +3,17 @@
  */
 package org.javahispano.javaleague.server.servlet;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.javahispano.javaleague.server.dao.JourneyDao;
+import org.javahispano.javaleague.server.dao.LeagueDao;
 
 /**
  * @author alfonso
@@ -19,9 +26,20 @@ public class ClasificationServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = -7232321012075894427L;
 	private final Logger logger;
+	private final LeagueDao leagueDao;
+	private final JourneyDao journeyDao;
 
 	@Inject
-	ClasificationServlet(Logger logger) {
+	ClasificationServlet(Logger logger, LeagueDao leagueDao,
+			JourneyDao journeyDao) {
 		this.logger = logger;
+		this.leagueDao = leagueDao;
+		this.journeyDao = journeyDao;
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
 	}
 }
